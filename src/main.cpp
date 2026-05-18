08a4f74c4f330a78f982bed36accf2c2650514a0
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-05-18 23:30:55 +0200

    Update the main function and Add thread to read every X seconds

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,36 @@
 #include "Telemetry.hpp"
 #include <iostream>
 
+#include "Telemetry.hpp"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
 int main() {
     DataLoader loader;
     std::string filepath = "data/telemetry_VER_Monza.csv";
 
     std::cout << "load data frome:" << filepath << std::endl;
 
-    const std::vector<TelemetryPoint> telemetryData = loader.loadCSV(filepath);
-
-    if (telemetryData.empty()) {
-        std::cerr << "Telemetry is empty" << std::endl;
+    std::vector<TelemetryPoint> telemetryData = loader.loadCSV(filepath);
+    if (telemetryData.empty())
         return EXIT_FAILURE;
-    }
 
     std::cout << "Succes:" << telemetryData.size() << " line loads.\n" << std::endl;
 
-    for (size_t i = 0; i < telemetryData.size(); i++) {
-        const auto& pt = telemetryData[i];
-        std::cout << "[Temps: " << pt.time_ms << " ms] "
+    auto start_time = std::chrono::steady_clock::now();
+
+    for (const auto& pt : telemetryData) {
+        auto target_time = start_time + std::chrono::milliseconds(static_cast<long long>(pt.time_ms));
+
+        std::this_thread::sleep_until(target_time);
+
+        std::cout << "\r[Real Time : " << pt.time_ms / 1000.0 << "s] "
                   << "Vitesse: " << pt.speed << " km/h | "
                   << "RPM: " << pt.rpm << " | "
                   << "Rapport: " << pt.nGear << " | "
-                  << "Frein: " << (pt.brake ? "OUI" : "NON") << std::endl;
+                  << "Frein: " << (pt.brake ? "OUI  " : "NON ") 
+                  << std::flush;
     }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
